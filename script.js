29ddad084d465ab9d212ac53174c18c46a5bb83f
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-04-26 17:51:05 +0600

    checks connection to the internet

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('ready mama');
+console.log("ready mama");
+
+const checkOnlineStatus = async () => {
+  try {
+    const online = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(online);
+    return online.status >= 200 && online.status < 300; // either true or false
+  } catch (err) {
+    return false; // definitely offline
+  }
+};
+
+setInterval(async () => {
+  const result = await checkOnlineStatus();
+  const statusDisplay = document.getElementById("status");
+  statusDisplay.textContent = result ? "Online" : "OFFline";
+}, 1000); // probably too often, try 30000 for every 30 seconds
+
+// forgot to include async load event listener in the video!
+window.addEventListener("load", async (event) => {
+  const statusDisplay = document.getElementById("status");
+  statusDisplay.textContent = (await checkOnlineStatus())
+    ? "Online"
+    : "OFFline";
+});
